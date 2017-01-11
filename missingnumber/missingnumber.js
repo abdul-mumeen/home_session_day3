022a7665aba72d66c2403c92ee4ebd40ae440ac5
phabbs <olasodeadeyemi@gmail.com>
2017-01-11 12:37:36 +0100

    committing working missingnumber.js and spec

---
+++ missingnumber/missingnumber.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+	findMissing: function(aArray,bArray)
+	{
+		
+	if (aArray.length === bArray.length)
+		{
+			return 0;
+		}
+		else
+		{		
+			aArray.sort();
+			bArray.sort();	
+			var len = (aArray.length > bArray.length) ? bArray.length : aArray.length;
+
+			for (var i = 0; i <= len; i++)
+			{
+				if(aArray[i] !== bArray[i])
+				{
+					if(aArray[i] === bArray[i + 1])
+					{
+						return bArray[i];
+					}
+					else
+					{
+						return aArray[i];
+					}
+				}
+				else
+				{
+					if (aArray.length === i || bArray.length === i)
+					{
+						if (bArray[i + 1] !== undefined)
+						{
+							return bArray[i + 1];
+						}
+						else
+						{
+							return aArray[i + 1];
+						}
+					}				
+				}
+			}
+		}
+	}
+};
